f9d23be344cd2ae05bd57a08a63dd5486441242d
Yuval Schwartz <yuval.orderdesk@gmail.com>
2021-01-07 19:36:06 -0500

    starting to work on the functionality

--- js/main.js
+++ js/main.js
@@ -1,13 +1,28 @@
 $( document ).ready(function() {
     console.log( "ready!" );
 
+    $('#serach-gif-button').on('click', (event) => {
+        console.log('button was clicked!');
+        
+        fetch('https://api.giphy.com/v1/gifs/search?api_key=UWdRaAwStnkl2S2IJ9L9JGiUWjtYYu8q&q=corona')
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            console.log('data array is:', data.data);
+            console.log('first result in the result array is:', data.data[0]);
+            console.log('url the originak images in the results array are:', data.data[0].images.original.url);
+
+            let html = "";
+
+            
+
+
+        });
 
 
 
 
+    });
 
 
-
-
-    
 });

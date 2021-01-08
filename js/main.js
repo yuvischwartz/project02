10cbda4dbce8ed42cddc89b407d47fcdec0d68f8
Yuval Schwartz <yuval.orderdesk@gmail.com>
2021-01-08 10:14:28 -0500

    having a problem with the searchquery

--- js/main.js
+++ js/main.js
@@ -1,9 +1,39 @@
 $( document ).ready(function() {
     console.log( "ready!" );
 
+    const fetchGifs = (searchQuery) => {
+        let gif_url = `https://api.giphy.com/v1/gifs/search?api_key=UWdRaAwStnkl2S2IJ9L9JGiUWjtYYu8q&q=${searchQuery}`
+        console.log(gif_url);
+        console.log(searchQuery);
+        
+        fetch(gif_url)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            console.log('data array is:', data.data);
+            console.log('first result in the result array is:', data.data[0]);
+            console.log('url the originak images in the results array are:', data.data[0].images.original.url);
+            
+            let html = ""; 
+            for(let img of data.data){
+                console.log(img.images.original.url);
+                let str = `<img src=${img.images.original.url} />`;
+                html = html + str;
+            }
+            
+            console.log(html);
+            $('#result').html(html);
+
+        });
+    };
+
     $('#serach-gif-button').on('click', (event) => {
         console.log('button was clicked!');
-        
+        let searchQuery = $('#search-input').val();
+        console.log(searchQuery);
+        fetchGifs();
+
+        /*
         fetch('https://api.giphy.com/v1/gifs/search?api_key=UWdRaAwStnkl2S2IJ9L9JGiUWjtYYu8q&q=corona')
         .then((response) => response.json())
         .then((data) => {
@@ -11,7 +41,7 @@ $( document ).ready(function() {
             console.log('data array is:', data.data);
             console.log('first result in the result array is:', data.data[0]);
             console.log('url the originak images in the results array are:', data.data[0].images.original.url);
-
+            
             let html = "";
             
             for(let img of data.data){
@@ -24,12 +54,12 @@ $( document ).ready(function() {
             console.log(html);
             $('#result').html(html);
 
-        });
-
-
-
-
+        });  */
     });
+
+
+
+
 
 
 });
